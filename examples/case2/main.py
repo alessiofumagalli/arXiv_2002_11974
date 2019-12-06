e9ca48b5c9b949d955bc6304f147f35e1c8c6975
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2019-12-06 13:50:49 +0100

    update case 2

--- examples/case2/main.py
+++ examples/case2/main.py
@@ -1,142 +1,22 @@
 import numpy as np
 import scipy.sparse as sps
+import matplotlib.pyplot as plt
+
 import porepy as pp
+
+from data import *
 
 import sys; sys.path.insert(0, "../../src/")
 from flow import Flow
+from import_grid import import_gb
 
 # ------------------------------------------------------------------------------#
 
-def bc_flag(g, data, tol):
-    b_faces = g.tags["domain_boundary_faces"].nonzero()[0]
-    b_face_centers = g.face_centers[:, b_faces]
+def main(name, gb, coarse=False):
 
-    b_dir_low = b_face_centers[0, :] > 1 - tol
-    b_dir_high = b_face_centers[0, :] < 0 + tol
-
-    # define the labels and values for the boundary faces
-    labels = np.array(["neu"] * b_faces.size)
-    labels[np.logical_or(b_dir_low, b_dir_high)] = "dir"
-
-    bc_val = np.zeros(g.num_faces)
-
-    bc_val[b_faces[b_dir_low]] = 1
-    bc_val[b_faces[b_dir_high]] = 4
-
-    return labels, bc_val
-
-# ------------------------------------------------------------------------------#
-
-def source(cell_centers):
-    return np.zeros(cell_centers.shape[1])
-
-# ------------------------------------------------------------------------------#
-
-def create_gb(file_name, mesh_size):
-    domain = {"xmin": 0, "xmax": 1, "ymin": 0, "ymax": 1}
-    network = pp.fracture_importer.network_2d_from_csv(file_name, domain=domain)
-
-    mesh_kwargs = {"mesh_size_frac": mesh_size, "mesh_size_min": mesh_size / 20}
-
-    # Generate a mixed-dimensional mesh
-    return network.mesh(mesh_kwargs)
-
-# ------------------------------------------------------------------------------#
-
-def set_flag(gb, tol):
-    # set the key for the low peremable fractures
-    gb.add_node_props("is_low", "frac_num")
-    for g, d in gb:
-        d["is_low"] = False
-        d["frac_num"] = -1
-        if g.dim == 1:
-
-            f_0 = (g.nodes[0, :] - 0.05)/(0.2200 - 0.05) - (g.nodes[1, :] - 0.4160)/(0.0624 - 0.4160)
-            if np.sum(np.abs(f_0)) < tol:
-                d["frac_num"] = 0
-                d["k_t"] = 1e4
-
-            f_1 = (g.nodes[0, :] - 0.05)/(0.2500 - 0.05) - (g.nodes[1, :] - 0.2750)/(0.1350 - 0.2750)
-            if np.sum(np.abs(f_1)) < tol:
-                d["frac_num"] = 1
-                d["k_t"] = 1e4
-
-            f_2 = (g.nodes[0, :] - 0.15)/(0.4500 - 0.15) - (g.nodes[1, :] - 0.6300)/(0.0900 - 0.6300)
-            if np.sum(np.abs(f_2)) < tol:
-                d["frac_num"] = 2
-                d["k_t"] = 1e4
-
-            f_3 = (g.nodes[0, :] - 0.15)/(0.4 - 0.15) - (g.nodes[1, :] - 0.9167)/(0.5 - 0.9167)
-            if np.sum(np.abs(f_3)) < tol:
-                d["frac_num"] = 3
-                d["is_low"] = True
-                d["k_t"] = 1e-4
-
-            f_4 = (g.nodes[0, :] - 0.65)/(0.849723 - 0.65) - (g.nodes[1, :] - 0.8333)/(0.167625 - 0.8333)
-            if np.sum(np.abs(f_4)) < tol:
-                d["frac_num"] = 4
-                d["is_low"] = True
-                d["k_t"] = 1e-4
-
-            f_5 = (g.nodes[0, :] - 0.70)/(0.849723 - 0.70) - (g.nodes[1, :] - 0.2350)/(0.167625 - 0.2350)
-            if np.sum(np.abs(f_5)) < tol:
-                d["frac_num"] = 5
-                d["k_t"] = 1e4
-
-            f_6 = (g.nodes[0, :] - 0.60)/(0.8500 - 0.60) - (g.nodes[1, :] - 0.3800)/(0.2675 - 0.3800)
-            if np.sum(np.abs(f_6)) < tol:
-                d["frac_num"] = 6
-                d["k_t"] = 1e4
-
-            f_7 = (g.nodes[0, :] - 0.35)/(0.8000 - 0.35) - (g.nodes[1, :] - 0.9714)/(0.7143 - 0.9714)
-            if np.sum(np.abs(f_7)) < tol:
-                d["frac_num"] = 7
-                d["k_t"] = 1e4
-
-            f_8 = (g.nodes[0, :] - 0.75)/(0.9500 - 0.75) - (g.nodes[1, :] - 0.9574)/(0.8155 - 0.9574)
-            if np.sum(np.abs(f_8)) < tol:
-                d["frac_num"] = 8
-                d["k_t"] = 1e4
-
-            f_9 = (g.nodes[0, :] - 0.15)/(0.4000 - 0.15) - (g.nodes[1, :] - 0.8363)/(0.9727 - 0.8363)
-            if np.sum(np.abs(f_9)) < tol:
-                d["frac_num"] = 9
-                d["k_t"] = 1e4
-        else:
-            d["k_t"] = 1
-
-    # we set know also the flag for the intersection, we need to go first through the
-    # 0-dim grids and set there the is low and after to the edges
-    gb.add_edge_props("is_low")
-    for _, d in gb.edges():
-        d["is_low"] = False
-        d["k_n"] = 1e4
-
-    for e, d in gb.edges():
-        gl, gh = gb.nodes_of_edge(e)
-        if gl.dim == 1 and gb.node_props(gl, "is_low"):
-            d["k_n"] = 1e-4
-        if gl.dim == 0 and gb.node_props(gh, "is_low"):
-            gb.set_node_prop(gl, "is_low", True)
-
-    # modify the key only for certain fractures
-    for e, d in gb.edges():
-        gl, gh = gb.nodes_of_edge(e)
-        if gl.dim == 0 and gb.node_props(gl, "is_low"):
-            d["k_n"] = 2./(1./1e-4+1./1e4)
-            d["is_low"] = True
-
-# ------------------------------------------------------------------------------#
-
-def main():
-
-    mesh_size = np.power(2., -3)
-    tol = 1e-6
     case = "case1"
-
-    # Define a fracture network in 2d
-    gb = create_gb("network.csv", mesh_size)
-    #pp.coarsening.coarsen(gb, "by_volume")
+    if coarse:
+        pp.coarsening.coarsen(gb, "by_volume")
 
     # the flow problem
     param = {
@@ -146,11 +26,7 @@ def main():
     }
     set_flag(gb, tol)
 
-    # exporter
-    save = pp.Exporter(gb, case, folder="solution")
-    save_vars = ["pressure", "P0_darcy_flux"]
-
-    # -- flow -- #
+    # ---- flow ---- #
     flow = Flow(gb)
     flow.set_data(param, bc_flag, source)
 
@@ -163,9 +39,26 @@ def main():
     # solve the problem
     flow.extract(x)
 
+    # output the solution
+    save = pp.Exporter(gb, case, folder="solution_"+name)
+    save_vars = ["pressure", "P0_darcy_flux"]
     save.write_vtk(save_vars)
 
 # ------------------------------------------------------------------------------#
 
 if __name__ == "__main__":
-    main()
+    tol = 1e-6
+    coarse = True
+
+    # ---- Simplex grid ---- #
+    mesh_size = np.power(2., -3)
+    file_name = "network_simple.csv"
+    gb = create_gb(file_name, mesh_size)
+
+    main("simplex", gb, coarse)
+
+    # ---- Cartesian cut grid ---- #
+    folder = "../../geometry/mesh_test_porepy/meshconuna/"
+    gb = import_gb(folder, 2, num_frac=1)
+
+    main("cartesian_cut", gb, coarse)
